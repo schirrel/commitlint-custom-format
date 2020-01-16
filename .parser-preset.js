50197f88fe666b8084e14d939066ab311b5b847c
Alan Schio <CoderSquirrel@users.noreply.github.com>
2020-01-16 15:42:24 -0400

    add regex Variations

--- .parser-preset.js
+++ .parser-preset.js
@@ -1,6 +1,17 @@
 module.exports = {
     parserOpts: {
-        headerPattern: /^\[(\w*)\] - #(\d+) - (.*)$/, // [type] - #numberofticket - Descriptions of the implementation
+        headerPattern: /^\[(\w*)\] - #(\d+) - (.*)$/, // [type] - #numberofticket - Descriptions of the implementation.      
         headerCorrespondence: ['type', 'references', 'subject']
     }
 };
+
+
+
+/* 
+Variations of regex
+^\[(\w*)\] - #(\d|[S\/N]+) - (.*)$  = accepting S/N instead of a ticket/reference number. Ex: [NOVO] - #S/N - tESTE
+
+^\[(\w*)\]\((\d|[S\/N]+)\):(.*)$ = Ex. [NOVO](2): tESTE   or [NOVO](S/N): tESTE
+
+
+*/
